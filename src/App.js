cffb094c894d5ba6e2e80373e0e65767a0ab388f
quinlans92 <82382254+quinlans92@users.noreply.github.com>
2023-08-29 17:15:26 +0100

    SingleProject page wip - Portfolio section

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import WeatherAppPic from './images/WeatherAppPic.png';
 import ThreadLairPic from './images/ThreadLairPic.png';
 import ReactRecipePic from './images/ReactRecipePic.png'
 import SingleProject from './Components/SingleProject';
+import weatherAppDevices from './images/WeatherAppDevised.png';
+import brainstorm from './images/brainstorm.png';
 
 function importAll(r) {
   let videos = {};
@@ -27,7 +29,11 @@ const videos = importAll(require.context('./videos', false, /\.mp4$/));
 const projects = [
   {
     id: 1,
-    dt: ["8 / 4 / 22"],
+    dt: ["8 / 4 / 2022"],
+    deviceImg: weatherAppDevices,
+    brainstorming: brainstorm,
+    ProblemsToSolve: ['Integration with APIs: I was required to find and integrate suitable APIs to fetch real-time weather data reliably and accurately.', 'Data Processing and Presentation: I was required to process the fetched data and present data in a user-friendly and visually appealing manner', 'Voice Interaction: Implementing speech recognition required me to learn new skills and solve challenges related to integrating the technology', 'UI Design and Interaction:I was required to design a fun UI that was very intuitive and kid friendly and come up with some engaging features to enhance the experience', 'Geo Location Accuracy: I was required to learn a new skill and integrating it smoothly into the app addressing challenges as they arise.'],
+    objective: ["Working as a member of a team I was required to research, previsualise and produce a motion graphics mockup of an application, based on a client’s brief. The mockup must include the core functionality of the application and show navigation and interactivity of its different sections and menus and any popup windows."],
     category: ["UI Design, ", "Motion Graphics"],
     Project: "Motion Mock Up",
     image: ThreadLairPic,
@@ -40,7 +46,11 @@ const projects = [
   },
   {
     id: 2,
-    dt: ["8 / 4 / 22"],
+    dt: ["8 / 4 / 2022"],
+    deviceImg: weatherAppDevices,
+    brainstorming: brainstorm,
+    ProblemsToSolve: ['INTEGRATION WITH APIs: I was required to find and integrate suitable APIs to fetch real-time weather data reliably and accurately.', 'DATA PROCESSING AND PRESENTAION: I was required to process the fetched data and present data in a user-friendly and visually appealing manner', 'VOICE INTERACTION: Implementing speech recognition required me to learn new skills and solve challenges related to integrating the technology', 'UI DESIGN AND INTERACTION:I was required to design a fun UI that was very intuitive and kid friendly and come up with some engaging features to enhance the experience', 'GEO LOCATION ACCURACY: I was required to learn a new skill and integrating it smoothly into the app addressing challenges as they arise.'],
+    objective: ["Design and develop an interactive experience that demonstrates programming, visualization combined with supporting hardware that facilitates user interaction or input and it must be based around Motion Tracking & Movement, Generative & Interactive Art or Real-Time Data Visualisation"],
     category: ["UI Design, ", "Graphic Design, ", "App Development"],
     Project: "   Kids Weather App",
     image: WeatherAppPic,
@@ -53,7 +63,11 @@ const projects = [
   },
   {
     id: 3,
-    dt: ["8 / 4 / 22"],
+    dt: ["8 / 4 / 2022"],
+    deviceImg: ThreadLairPic,
+    brainstorming: brainstorm,
+    ProblemsToSolve: ['Integration with APIs: I was required to find and integrate suitable APIs to fetch real-time weather data reliably and accurately.', 'Data Processing and Presentation: I was required to process the fetched data and present data in a user-friendly and visually appealing manner', 'Voice Interaction: Implementing speech recognition required me to learn new skills and solve challenges related to integrating the technology', 'UI Design and Interaction:I was required to design a fun UI that was very intuitive and kid friendly and come up with some engaging features to enhance the experience', 'Geo Location Accuracy: I was required to learn a new skill and integrating it smoothly into the app addressing challenges as they arise.'],
+    objective: ["Create CRUD recipe list React Application that allows me to log my favourite recipies. The details should, at a minimun, include the recipe title, a list of ingredients and the cooking/baking."],
     category: ["UI Design, ", "Web Development, ", "App Development"],
     Project: " React Recipe App",
     image: ReactRecipePic,
--- src/Components/SingleProject.js
+++ src/Components/SingleProject.js
@@ -3,7 +3,8 @@ import { useParams, Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faHome, faHeart } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons'; // Import the GitHub icon
-import weatherAppDevices from '../images/WeatherAppDevised.png'
+import backGround from '../images/background.jpg'
+
 
 
 
@@ -20,7 +21,7 @@ const SingleProject = ({ projects }) => {
         setIsHovered(false);
     };
 
-    const textColor = isHovered ? '#A65151' : '#000';
+    const textColor = isHovered ? '#fff' : '#000';
 
 
 
@@ -29,94 +30,130 @@ const SingleProject = ({ projects }) => {
         item.id === Number(urlParameters.projectID)
     ));
 
+    console.log("projectToDisplay:", projectToDisplay);
+
     return (
         <div className="container-fluid" style={{ paddingTop: '100px', backgroundColor: '#A65151' }}>
             <div className="row">
-                <div key={projectToDisplay.id}>
-                    <h2>{projectToDisplay.Project}</h2>
+                <div className="col-md-12 text-center">
+                    <h1 style={{ color: '#fff' }}>{projectToDisplay.Project}</h1>
+                    <br /><br /><br />
+                    <Link to={`/projects/${projectToDisplay.id}`}>
+                        <img
+                            src={projectToDisplay.deviceImg}
+                            alt="Image"
+                            className="img-fluid"
+                        />
+                    </Link>
                 </div>
 
+                <div className="col-md-12 text-center mt-4">
+                    <h3 style={{ fontWeight: 'bold', color: '#fff', marginTop: '30px', marginBottom: '30px' }}>Objective</h3>
+                    {projectToDisplay.objective.map((objective, index) => (
+                        <p key={index} style={{ fontSize: '20px', color: '#fff', paddingLeft: '30px', paddingRight: '30px', marginBottom: '30px' }}>{objective}</p>
+                    ))}
+                </div>
 
-                <img src={weatherAppDevices} alt="weatherAppDevices" />
-                <div className="col-md-12">
-                    <div className="d-flex flex-column align-items-center align-md-start">
-                        {projectToDisplay.dt.map((dt, index) => (
-                            <p key={index} style={{ fontSize: '24px', }}>Date of Project: {dt}</p>
-                        ))}
-
-                        <p style={{ fontSize: '24px', fontWeight: 'bold', float: 'left', marginTop: '20px', marginBottom: '40px' }}>Demo</p>
-                        <video
-                            controls
-                            style={{
-                                maxWidth: '100%',
-                                height: 'auto',
-                                border: '3px solid black',
-                                borderRadius: '5px',
-                            }}
-                        >
-                            <source src={projectToDisplay.video} type="video/mp4" />
-                        </video>
-                        {projectToDisplay.isCodeProject && (
-
-                            <div className='codeIcons'>
-                                <h3>
-                                    <a
-                                        href="https://github.com/quinlans92/kidsWeatherApp_bySamantha"
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        style={{
-                                            textDecoration: 'none',
-                                            color: textColor,
-                                            display: 'flex',
-                                            alignItems: 'center',
-                                            transition: 'color 0.3s',
-                                        }}
-                                        onMouseEnter={handleMouseEnter}
-                                        onMouseLeave={handleMouseLeave}
-                                    >
-                                        <FontAwesomeIcon
-                                            icon={faGithub}
-                                            style={{
-                                                color: textColor,
-                                                width: '40px',
-                                                height: '40px',
-                                                marginRight: '10px',
-                                                marginTop: '10px',
-                                                transition: 'color 0.3s',
-                                            }}
-                                        />
-                                        <span
-                                            style={{
-                                                borderBottom: `2px solid ${textColor}`,
-                                                transition: 'border-color 0.3s',
-                                            }}
-                                        >
-                                            Check out the code on Github
-                                        </span>
-                                    </a>
-                                </h3>
+                <div className="col-md-12 text-center mt-4">
+                    <h3 style={{ fontWeight: 'bold', color: '#fff', marginTop: '30px', marginBottom: '30px' }}>Brainstorming</h3>
+                    <Link to={`/projects/${projectToDisplay.id}`}>
+                        <img
+                            src={projectToDisplay.brainstorming}
+                            alt="Brainstorming"
+                            className="img-fluid"
+                            style={{ width: '80%', height: 'auto' }}
+                        />
+                    </Link>
+                </div>
+                <div className='d-flex justify-content-center'>
+                    <div className='col-md-8'>
+                        <div className='text-center'>
+                            <h3 style={{ fontWeight: 'bold', color: '#fff', marginTop: '30px', marginBottom: '30px' }}>Identified Challenges</h3>
+                        </div>
+                        {projectToDisplay.ProblemsToSolve.map((ProblemsToSolve, index) => (
+                            <div key={index} className="bg-light p-3 rounded mb-3">
+                                <p style={{ color: '#A65151', fontWeight: 'bold', marginBottom: '30px' }} className="mb-0">{ProblemsToSolve}</p>
                             </div>
-                        )}
-                    </div>
-                </div>
-                <div className="col-md-6">
-                    <div className="d-flex flex-column align-items-center align-md-start">
-                        <p style={{ color: '#000', fontSize: '24px', fontWeight: 'bold', paddingLeft: '40px', paddingRight: '40px', marginTop: '20px', marginBottom: '40px' }}>Features</p>
-                        {projectToDisplay.features.map((features, index) => (
-                            <p key={index} style={{ fontSize: '24px', }}>{features}</p>
                         ))}
                     </div>
                 </div>
-                <div className='col-md-12 d-flex flex-column align-items-center align-md-start bg' style={{ color: '#000', fontSize: '26px', fontWeight: 'bold', padding: '10px', textTransform: 'uppercase', marginTop: '40px' }}>
-                    <span>Process</span>
-                </div>
+
+                <p style={{ fontSize: '24px', fontWeight: 'bold', float: 'left', marginTop: '20px', marginBottom: '40px' }}>Demo</p>
+                <video
+                    controls
+                    style={{
+                        maxWidth: '100%',
+                        height: 'auto',
+                        border: '3px solid black',
+                        borderRadius: '5px',
+                    }}
+                >
+                    <source src={projectToDisplay.video} type="video/mp4" />
+                </video>
+                {projectToDisplay.isCodeProject && (
+
+                    <div className='codeIcons'>
+                        <h3>
+                            <a
+                                href="https://github.com/quinlans92/kidsWeatherApp_bySamantha"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                style={{
+                                    textDecoration: 'none',
+                                    color: textColor,
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    transition: 'color 0.3s',
+                                }}
+                                onMouseEnter={handleMouseEnter}
+                                onMouseLeave={handleMouseLeave}
+                            >
+                                <FontAwesomeIcon
+                                    icon={faGithub}
+                                    style={{
+                                        color: textColor,
+                                        width: '40px',
+                                        height: '40px',
+                                        marginRight: '10px',
+                                        marginTop: '10px',
+                                        transition: 'color 0.3s',
+                                    }}
+                                />
+                                <span
+                                    style={{
+                                        borderBottom: `2px solid ${textColor}`,
+                                        transition: 'border-color 0.3s',
+                                    }}
+                                >
+                                    Check out the code on Github
+                                </span>
+                            </a>
+                        </h3>
+                    </div>
+                )}
+            </div>
+            <div className="col-md-6">
                 <div className="d-flex flex-column align-items-center align-md-start">
-                    <p style={{ color: '#000', fontSize: '24px', fontWeight: 'bold', paddingLeft: '40px', paddingRight: '40px', marginTop: '40px', marginBottom: '40px' }}>Features</p>
-                    {projectToDisplay.Process.map((Process, index) => (
-                        <p key={index} style={{ fontSize: '24px', }}>{Process}</p>
+                    <p style={{ color: '#000', fontSize: '24px', fontWeight: 'bold', paddingLeft: '40px', paddingRight: '40px', marginTop: '20px', marginBottom: '40px' }}>Features</p>
+                    {projectToDisplay.features.map((features, index) => (
+                        <p key={index} style={{ fontSize: '24px', }}>{features}</p>
                     ))}
                 </div>
             </div>
+            <div className='col-md-12 d-flex flex-column align-items-center align-md-start bg' style={{ color: '#000', fontSize: '26px', fontWeight: 'bold', padding: '10px', textTransform: 'uppercase', marginTop: '40px' }}>
+                <span>Process</span>
+            </div>
+            <div className="d-flex flex-column align-items-center align-md-start">
+                <p style={{ color: '#000', fontSize: '24px', fontWeight: 'bold', paddingLeft: '40px', paddingRight: '40px', marginTop: '40px', marginBottom: '40px' }}>Features</p>
+                {projectToDisplay.Process.map((Process, index) => (
+                    <p key={index} style={{ fontSize: '24px', }}>{Process}</p>
+                ))}
+            </div>
+
+            <h3>Date of Project</h3><br />
+            {projectToDisplay.dt.map((dt, index) => (
+                <p key={index} style={{ fontSize: '24px', }}> {dt}</p>
+            ))}
             <Link to={`/Projects`} className="align-self-start">
                 <FontAwesomeIcon
                     style={{
