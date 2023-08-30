f8b32b08e2fcd95df397989d383c530d0d6e5465
quinlans92 <82382254+quinlans92@users.noreply.github.com>
2023-08-30 16:00:31 +0100

    kidsWeatherApp portfolio page almost complete

--- src/Components/SingleProject.js
+++ src/Components/SingleProject.js
@@ -78,98 +78,189 @@ const SingleProject = ({ projects }) => {
                     </div>
                 </div>
 
-                <p style={{ fontSize: '24px', fontWeight: 'bold', float: 'left', marginTop: '20px', marginBottom: '40px' }}>Demo</p>
-                <video
-                    controls
-                    style={{
-                        maxWidth: '100%',
-                        height: 'auto',
-                        border: '3px solid black',
-                        borderRadius: '5px',
-                    }}
-                >
-                    <source src={projectToDisplay.video} type="video/mp4" />
-                </video>
-                {projectToDisplay.isCodeProject && (
-
-                    <div className='codeIcons'>
-                        <h3>
-                            <a
-                                href="https://github.com/quinlans92/kidsWeatherApp_bySamantha"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                style={{
-                                    textDecoration: 'none',
-                                    color: textColor,
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    transition: 'color 0.3s',
-                                }}
-                                onMouseEnter={handleMouseEnter}
-                                onMouseLeave={handleMouseLeave}
-                            >
-                                <FontAwesomeIcon
-                                    icon={faGithub}
-                                    style={{
-                                        color: textColor,
-                                        width: '40px',
-                                        height: '40px',
-                                        marginRight: '10px',
-                                        marginTop: '10px',
-                                        transition: 'color 0.3s',
-                                    }}
+                <div className="col-md-12 text-center mt-4">
+                    <h3 style={{ fontWeight: 'bold', color: '#fff', marginTop: '30px', marginBottom: '30px' }}>Early Sketch</h3>
+                    <div className="d-flex justify-content-center">
+                        {projectToDisplay.sketch.map((sketchImage, index) => (
+                            <Link key={index} to={`/projects/${projectToDisplay.id}`}>
+                                <img
+                                    src={sketchImage}
+                                    alt={`sketch-${index}`}
+                                    className="img-fluid"
+                                    style={{ width: '90%', height: 'auto', margin: '10px', border: '10px solid white' }}
                                 />
-                                <span
-                                    style={{
-                                        borderBottom: `2px solid ${textColor}`,
-                                        transition: 'border-color 0.3s',
-                                    }}
-                                >
-                                    Check out the code on Github
-                                </span>
-                            </a>
-                        </h3>
+                            </Link>
+                        ))}
                     </div>
-                )}
-            </div>
-            <div className="col-md-6">
-                <div className="d-flex flex-column align-items-center align-md-start">
-                    <p style={{ color: '#000', fontSize: '24px', fontWeight: 'bold', paddingLeft: '40px', paddingRight: '40px', marginTop: '20px', marginBottom: '40px' }}>Features</p>
-                    {projectToDisplay.features.map((features, index) => (
-                        <p key={index} style={{ fontSize: '24px', }}>{features}</p>
+                </div>
+                <div className="col-md-12 text-center mt-4">
+                    <h3 style={{ fontWeight: 'bold', color: '#fff', marginTop: '30px', marginBottom: '30px' }}>Mockup</h3>
+                    <Link to={`/projects/${projectToDisplay.id}`}>
+                        <img
+                            src={projectToDisplay.bwMockUp}
+                            alt="Brainstorming"
+                            className="img-fluid"
+                            style={{ width: '80%', height: 'auto' }}
+                        />
+                    </Link>
+                </div>
+                <div className="col-md-12 text-center mt-4">
+                    <h3 style={{ fontWeight: 'bold', color: '#fff', marginTop: '30px', marginBottom: '30px' }}>Color Palettes Considered</h3>
+                    <Link to={`/projects/${projectToDisplay.id}`}>
+                        <img
+                            src={projectToDisplay.colorPallete}
+                            alt="Brainstorming"
+                            className="img-fluid"
+                            style={{ width: '80%', height: 'auto' }}
+                        />
+                    </Link>
+                    {projectToDisplay.colorPalleteDes.map((colorPalleteDes, index) => (
+                        <p key={index} style={{ fontSize: '20px', color: '#fff', paddingLeft: '30px', paddingRight: '30px', marginBottom: '30px', marginTop: '30px' }}>{colorPalleteDes}</p>
                     ))}
                 </div>
+                <div className="col-md-12 text-center mt-4">
+                    <h3 style={{ fontWeight: 'bold', color: '#fff', marginTop: '30px', marginBottom: '30px' }}>Fonts Considered</h3>
+                    <Link to={`/projects/${projectToDisplay.id}`}>
+                        <img
+                            src={projectToDisplay.font}
+                            alt="Brainstorming"
+                            className="img-fluid"
+                            style={{ width: '80%', height: 'auto' }}
+                        />
+                    </Link>
+                    {projectToDisplay.fontDesc.map((fontDesc, index) => (
+                        <p key={index} style={{ fontSize: '20px', color: '#fff', paddingLeft: '30px', paddingRight: '30px', marginBottom: '30px', marginTop: '30px', }}>{fontDesc}</p>
+                    ))}
+                </div>
+                <div className="col-md-12 text-center mt-4">
+                    <h3 style={{ fontWeight: 'bold', color: '#fff', marginTop: '30px', marginBottom: '30px' }}>Final Mockup</h3>
+                    <Link to={`/projects/${projectToDisplay.id}`}>
+                        <img
+                            src={projectToDisplay.finalMocup}
+                            alt="Brainstorming"
+                            className="img-fluid"
+                            style={{ width: '80%', height: 'auto' }}
+                        />
+                    </Link>
+                    {projectToDisplay.finalMockUpDesc.map((finalMockUpDesc, index) => (
+                        <p key={index} style={{ fontSize: '20px', color: '#fff', paddingLeft: '30px', paddingRight: '30px', marginBottom: '30px', marginTop: '30px', }}>{finalMockUpDesc}</p>
+                    ))}
+                </div>
+                <div className='col-md-12 d-flex justify-content-center'>
+                    <h3 style={{ fontWeight: 'bold', color: '#fff', marginTop: '30px', marginBottom: '30px' }}>Demo</h3>
+                </div>
+                <div className='col-md-12 d-flex justify-content-center'>
+                    <video
+                        controls
+                        style={{
+                            maxWidth: '100%',
+                            height: 'auto',
+                            border: '3px solid black',
+                            borderRadius: '5px',
+                        }}
+                    >
+                        <source src={projectToDisplay.video} type="video/mp4" />
+                    </video>
+                </div>
+                <div className='col-md-12 d-flex justify-content-center'>
+                    {projectToDisplay.isCodeProject && (
+                        <div className='codeIcons'>
+                            <h3>
+                                <a
+                                    href="https://github.com/quinlans92/kidsWeatherApp_bySamantha"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    style={{
+                                        textDecoration: 'none',
+                                        color: textColor,
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        transition: 'color 0.3s',
+                                    }}
+                                    onMouseEnter={handleMouseEnter}
+                                    onMouseLeave={handleMouseLeave}
+                                >
+                                    <FontAwesomeIcon
+                                        icon={faGithub}
+                                        style={{
+                                            color: textColor,
+                                            width: '40px',
+                                            height: '40px',
+                                            marginRight: '10px',
+                                            marginTop: '10px',
+                                            transition: 'color 0.3s',
+                                        }}
+                                    />
+                                    <span
+                                        style={{
+                                            borderBottom: `2px solid ${textColor}`,
+                                            transition: 'border-color 0.3s',
+                                        }}
+                                    >
+                                        Check out the code on Github
+                                    </span>
+                                </a>
+                            </h3>
+                        </div>
+                    )}
+                </div>
+
+
             </div>
-            <div className='col-md-12 d-flex flex-column align-items-center align-md-start bg' style={{ color: '#000', fontSize: '26px', fontWeight: 'bold', padding: '10px', textTransform: 'uppercase', marginTop: '40px' }}>
-                <span>Process</span>
-            </div>
-            <div className="d-flex flex-column align-items-center align-md-start">
-                <p style={{ color: '#000', fontSize: '24px', fontWeight: 'bold', paddingLeft: '40px', paddingRight: '40px', marginTop: '40px', marginBottom: '40px' }}>Features</p>
-                {projectToDisplay.Process.map((Process, index) => (
-                    <p key={index} style={{ fontSize: '24px', }}>{Process}</p>
-                ))}
+            <div className="row">
+                <div className="col-md-6 d-flex justify-content-center">
+                    <div className="col-md-10">
+                        <div className="text-center">
+                            <h3 style={{ fontWeight: 'bold', color: '#fff', marginTop: '30px', marginBottom: '30px' }}>Features</h3>
+                        </div>
+                        <div className="d-flex flex-column align-items-center">
+                            {projectToDisplay.features.map((feature, index) => (
+                                <p key={index} style={{ color: '#fff', fontWeight: 'bold', marginBottom: '0px' }}>{feature}</p>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6 d-flex justify-content-center">
+                    <div className="col-md-10">
+                        <div className="text-center">
+                            <h3 style={{ fontWeight: 'bold', color: '#fff', marginTop: '30px', marginBottom: '30px' }}>Software</h3>
+                        </div>
+                        <div className="d-flex flex-column align-items-center">
+                            {projectToDisplay.Software.map((Software, index) => (
+                                <p key={index} style={{ color: '#fff', fontWeight: 'bold', marginBottom: '0px' }}>{Software}</p>
+
+                            ))}
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            <h3>Date of Project</h3><br />
-            {projectToDisplay.dt.map((dt, index) => (
-                <p key={index} style={{ fontSize: '24px', }}> {dt}</p>
-            ))}
-            <Link to={`/Projects`} className="align-self-start">
-                <FontAwesomeIcon
-                    style={{
-                        padding: '10px',
-                        backgroundColor: 'transparent',
-                        fontSize: '1.5rem',
-                        color: 'black',
-                        cursor: 'pointer',
-                        outline: 'none',
-                        borderRadius: '5px',
-                        border: '1px solid white',
-                    }}
-                    icon={faArrowLeft}
-                />
-            </Link>
+
+            <div>
+                <h3>Date of Project</h3><br />
+                {projectToDisplay.dt.map((dt, index) => (
+                    <p key={index} style={{ fontSize: '24px' }}>{dt}</p>
+                ))}
+                <Link to={`/Projects`} className="align-self-start">
+                    <FontAwesomeIcon
+                        style={{
+                            padding: '10px',
+                            backgroundColor: 'transparent',
+                            fontSize: '1.5rem',
+                            color: 'black',
+                            cursor: 'pointer',
+                            outline: 'none',
+                            borderRadius: '5px',
+                            border: '1px solid white',
+                        }}
+                        icon={faArrowLeft}
+                    />
+                </Link>
+            </div>
         </div>
+
+
+
     );
 
 
